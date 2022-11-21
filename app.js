24ec52bc435ae54247be3e270230b466fa6d0058
Alex Smith <109144005+AlexMSm@users.noreply.github.com>
2022-11-21 11:34:47 +0000

    endpoint info added

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ const router = require("./router");
 app.use("/api", router);
 
 app.all("/*", (req, res) => {
-  res.status(404).send({ message: "Bad path" });
+  res.status(404).send({
+    message:
+      "Bad path - only two endpoints available: \n 1. /api/image for general image recognition model. \n 2. /api/rps for rock paper scissors recognition model. \n Both expect body of {body: url} where url is a string",
+  });
 });
 
 module.exports = app;
